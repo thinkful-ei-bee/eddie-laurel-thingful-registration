f8b042d40654ceab94394d1695b60f28b2f611e7
Eddie Bueno <eddiebueno93@gmail.com>
2019-04-18 12:11:04 -0700

    finished user validation

--- server/src/users/users-service.js
+++ server/src/users/users-service.js
@@ -1,15 +1,28 @@
-const UsersService = {
-    validatePassword(password) {
-        if (password.length < 8) {
-            return 'Password be longer than 8 characters'
-        }
-        if (password.length > 72) {
-            return 'Password be less than 72 characters'
-        }
-        if (password.startsWith(' ') || password.endsWith(' ')) {
-     return 'Password must not start or end with empty spaces'
-   }
-    },
-}
+'use strict';
+const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/;
 
-module.exports = UsersService
+const UsersService = {
+  hasUserWithUserName(db,user_name){
+    return db('thingful_users')
+      .where({user_name})
+      .first()
+      .then(user=>!!user);
+  },
+  validatePassword(password) {
+    if (password.length < 8) {
+      return 'Password be longer than 8 characters';
+    }
+    if (password.length > 72) {
+      return 'Password be less than 72 characters';
+    }
+    if (password.startsWith(' ') || password.endsWith(' ')) {
+      return 'Password must not start or end with empty spaces';
+    }
+    if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
+      return 'Password must contain 1 upper case, lower case, number and special character';
+    }
+    return null;
+  },
+};
+
+module.exports = UsersService;
